440d14cf6ed66c8ec55ec30f383b89f890e90628
Satyam Kumar <146471886+satyamkr203@users.noreply.github.com>
2025-05-23 12:25:43 +0530

    Update models.py

--- backend/database/models.py
+++ backend/database/models.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 class Document(Base):
     __tablename__ = "documents"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)  # Auto-incrementing Integer ID
+    id = Column(Integer, primary_key=True, autoincrement=True)  
     name = Column(String, nullable=False)
     path = Column(String, nullable=False)
-    upload_date = Column(DateTime, default=datetime.utcnow)  # Automatically sets current time if no value is provided
+    upload_date = Column(DateTime, default=datetime.utcnow)  
     size = Column(Integer)
 
 def init_db(engine):
